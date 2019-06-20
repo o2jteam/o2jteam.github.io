e3571df409ec7a8f631a6bfe74e0d6e999d4b94a
piaoxiaobo <643209899@qq.com>
2019-06-21 07:30:29 +0800

    update

--- js/ocean.js
+++ js/ocean.js
@@ -251,98 +251,79 @@
     }, mobileNavAnimDuration);
   };
 
-  var flag = false;
-  var cur = {
-    x: 0,
-    y: 0
-  };
-  var nx, ny, dx, dy, x, y;
-
-  function down() {
-    flag = true;
-    var touch;
-    if (event.touches) {
-      touch = event.touches[0];
-    } else {
-      touch = event;
-    }
-    cur.x = touch.clientX;
-    cur.y = touch.clientY;
-    dx = landlord.offsetLeft;
-    dy = landlord.offsetTop;
-  }
-
-  function move() {
-    if (flag) {
-      var touch;
-      if (event.touches) {
-        touch = event.touches[0];
-      } else {
-        touch = event;
+  var cont = $("#landlord");
+  var contW = $("#landlord").width();
+  var contH = $("#landlord").height();
+  var startX, startY, sX, sY, moveX, moveY, disX, disY;
+  var winW = $(window).width();
+  var winH = $(window).height();
+  cont.on({
+    //绑定事件
+    touchstart: function(e) {
+      startX = e.originalEvent.targetTouches[0].pageX; //获取点击点的X坐标
+      startY = e.originalEvent.targetTouches[0].pageY; //获取点击点的Y坐标
+      //console.log("startX="+startX+"************startY="+startY);
+      sX = $(this).offset().left; //相对于当前窗口X轴的偏移量
+      sY = $(this).offset().top; //相对于当前窗口Y轴的偏移量
+      //console.log("sX="+sX+"***************sY="+sY);
+      leftX = startX - sX; //鼠标所能移动的最左端是当前鼠标距div左边距的位置
+      rightX = winW - contW + leftX; //鼠标所能移动的最右端是当前窗口距离减去鼠标距div最右端位置
+      topY = startY - sY; //鼠标所能移动最上端是当前鼠标距div上边距的位置
+      bottomY = winH - contH + topY; //鼠标所能移动最下端是当前窗口距离减去鼠标距div最下端位置
+    },
+    touchmove: function(e) {
+      e.preventDefault();
+      moveX = e.originalEvent.targetTouches[0].pageX; //移动过程中X轴的坐标
+      moveY = e.originalEvent.targetTouches[0].pageY; //移动过程中Y轴的坐标
+      //console.log("moveX="+moveX+"************moveY="+moveY);
+      if (moveX < leftX) {
+        moveX = leftX;
       }
-      nx = touch.clientX - cur.x;
-      ny = touch.clientY - cur.y;
-      x = dx + nx;
-      y = dy + ny;
-      landlord.style.left = x + "px";
-      landlord.style.top = y + "px";
-      //阻止页面的滑动默认事件
-      document.addEventListener(
-        "touchmove",
-        function() {
-          event.preventDefault();
-        },
-        { passive: false }
-      );
+      if (moveX > rightX) {
+        moveX = rightX;
+      }
+      if (moveY < topY) {
+        moveY = topY;
+      }
+      if (moveY > bottomY) {
+        moveY = bottomY;
+      }
+      $(this).css({
+        left: moveX + sX - startX,
+        top: moveY + sY - startY
+      });
+    },
+    mousedown: function(ev) {
+      var patch = parseInt($(this).css("height")) / 2;
+      //console.log(patch);
+      $(this).mousemove(function(ev) {
+        var oEvent = ev || event;
+        //console.log(oEvent.target);
+        var oX = oEvent.clientX;
+        var oY = oEvent.clientY;
+        var t = oY - patch;
+        var l = oX - patch;
+        var w = $(window).width() - $(this).width();
+        var h = $(window).height() - $(this).height();
+        if (t < 0) {
+          t = 0;
+        }
+        if (l < 0) {
+          l = 0;
+        }
+        if (t > h) {
+          t = h;
+        }
+        if (l > w) {
+          l = w;
+        }
+        $(this).css({ top: t, left: l });
+      });
+      $(this).mouseup(function() {
+        $(this).unbind("mousemove");
+      });
     }
-  }
-  //鼠标释放时候的函数
-  function end() {
-    flag = false;
-  }
-  var landlord = document.getElementById("landlord");
-  landlord.addEventListener(
-    "mousedown",
-    function() {
-      down();
-    },
-    false
-  );
-  landlord.addEventListener(
-    "touchstart",
-    function() {
-      down();
-    },
-    false
-  );
-  landlord.addEventListener(
-    "mousemove",
-    function() {
-      move();
-    },
-    false
-  );
-  landlord.addEventListener(
-    "touchmove",
-    function() {
-      move();
-    },
-    { passive: false }
-  );
-  document.body.addEventListener(
-    "mouseup",
-    function() {
-      end();
-    },
-    false
-  );
-  landlord.addEventListener(
-    "touchend",
-    function() {
-      end();
-    },
-    false
-  );
+  });
 
   $("#menubtn").on("click", function() {
     if (isMobileNavAnim) return;
